a59aba7f3a335462f965712217fe0f3ab8405146
chikovani2001 <chikovani2001@inbox.ru>
2021-02-23 22:06:22 +0400

    message

--- js/timer.js
+++ js/timer.js
@@ -18,6 +18,9 @@ const COLOR_CODES = {
   }
 };
 
+
+
+
 const TIME_LIMIT = 60;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
@@ -48,7 +51,94 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-// startTimer();
+
+let start = document.querySelector('#start')
+start.onclick = function(){
+  startTimer();
+}
+
+let finish = document.querySelector('#finish')
+finish.onclick = function() {
+  window.location.reload();
+}
+
+let stop = document.querySelector('#stop')
+stop.onclick = function(){
+  stopTimer();
+  document.getElementById("name").readOnly = true;
+  document.getElementById("lastName").readOnly = true;
+  document.getElementById("country").readOnly = true;
+  document.getElementById("city").readOnly = true;
+  document.getElementById("animal").readOnly = true;
+  document.getElementById("plant").readOnly = true;
+}
+
+let more = document.querySelector('#more')
+more.onclick = function(){
+  resetTimer();
+
+} 
+let stop1 = document.querySelector('#stop1')
+stop1.onclick = function(){
+  stopTimer();
+  document.getElementById("name1").readOnly = true;
+  document.getElementById("lastName1").readOnly = true;
+  document.getElementById("country1").readOnly = true;
+  document.getElementById("city1").readOnly = true;
+  document.getElementById("animal1").readOnly = true;
+  document.getElementById("plan1t").readOnly = true;
+}
+
+let more1 = document.querySelector('#more1')
+more1.onclick = function(){
+  // resetTimer();
+}
+let stop2 = document.querySelector('#stop2')
+stop2.onclick = function(){
+  stopTimer();
+  document.getElementById("name2").readOnly = true;
+  document.getElementById("lastName2").readOnly = true;
+  document.getElementById("country2").readOnly = true;
+  document.getElementById("city2").readOnly = true;
+  document.getElementById("animal2").readOnly = true;
+  document.getElementById("plant2").readOnly = true;
+}
+
+let more2 = document.querySelector('#more2')
+more2.onclick = function(){
+  // resetTimer();
+}
+let stop3 = document.querySelector('#stop3')
+stop3.onclick = function(){
+  stopTimer();
+  document.getElementById("name3").readOnly = true;
+  document.getElementById("lastName3").readOnly = true;
+  document.getElementById("country3").readOnly = true;
+  document.getElementById("city3").readOnly = true;
+  document.getElementById("animal3").readOnly = true;
+  document.getElementById("plant3").readOnly = true;
+}
+
+let more3 = document.querySelector('#more3')
+more3.onclick = function(){
+  // resetTimer();
+}
+let stop4 = document.querySelector('#stop4')
+stop.onclick = function(){
+  stopTimer();
+  document.getElementById("name4").readOnly = true;
+  document.getElementById("lastName4").readOnly = true;
+  document.getElementById("country4").readOnly = true;
+  document.getElementById("city4").readOnly = true;
+  document.getElementById("animal4").readOnly = true;
+  document.getElementById("plant4").readOnly = true;
+}
+
+let more4 = document.querySelector('#more4')
+more4.onclick = function(){
+  // resetTimer();
+}
+
 
 function onTimesUp() {
   clearInterval(timerInterval);
@@ -69,6 +159,13 @@ function startTimer() {
     }
   }, 1000);
 }
+function stopTimer(){
+  clearInterval(timerInterval)
+}
+function resetTimer(){
+  clearTimeout();
+}
+
 
 function formatTime(time) {
   const minutes = Math.floor(time / 60);
@@ -114,6 +211,9 @@ function setCircleDasharray() {
     .setAttribute("stroke-dasharray", circleDasharray);
 }
 
+function resetTimer(){ 
+  window.location.reload();
+}
 
 
 
@@ -158,5 +258,6 @@ function randomLeter(letters) {
    return letters[Math.floor(Math.random() * letters.length)];
 }
 
-console.log(randomLeter(letters)) 
-document.getElementById("demo").innerHTML = randomLeter(letters);
+document.querySelector("#demo").innerHTML = randomLeter(letters);
+console.log(randomLeter(letters))
+
